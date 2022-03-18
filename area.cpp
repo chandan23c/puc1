b2e768f3a312e98656ed442a27e776e5b046c966
Chandan C <45237917+chandan23c@users.noreply.github.com>
2022-03-18 10:46:30 +0530

    Create area.cpp

---
+++ area.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>
+#include <conio.h>
+#include <iomanip.h>
+
+void main ()
+ {
+  float area, circum, rad;
+  clrscr ();
+  cout<<"enter the radius of the circle:";
+  cin>>rad;
+  area= 3.14*rad*rad;
+  circum= 2*3.14*rad;
+  cout<<"area of the circle="<<area<<endl;
+  cout<<"circumference="<<circum<<endl;
+  getch ();
+ }
